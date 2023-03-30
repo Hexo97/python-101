62a1c027658fe85504c3d0be0989cc862eba5163
Hexo97 <57594288+Hexo97@users.noreply.github.com>
2023-03-30 08:08:35 +0300

    adding variables.py file

---
+++ variables.py
@@ -0,0 +1,40 @@
+# Question 1
+# Write a Python program that declares a variable called name and assigns it the value "John". Print the value of the variable
+
+#######################################################################################################################
+
+# Question 2
+# Write a Python program that declares two variables called num1 and num2 and assigns them the values 10 and 20, respectively. 
+
+# Print the values of both variables and then swap their values using a third variable called temp. 
+
+# Print the new values of num1 and num2.
+
+#######################################################################################################################
+
+# Question 3
+# Write a Python program that declares three variables called a, b, and c and assigns them the values 1, "Hello", and True, respectively. 
+
+# Print the values of all three variables and their types.
+
+#######################################################################################################################
+
+# Question 4
+# Write a Python program that prompts the user to enter a number and assigns it to a variable called x. 
+
+# Print the value of the variable and its type.
+
+#######################################################################################################################
+
+# Question 5
+# Write a Python program that prompts the user to enter a number and assigns it to a variable called x. 
+
+# Print the value of the variable and its type.
+
+#######################################################################################################################
+
+# Question 6
+# Write a Python program that prompts the user to enter a number and assigns it to a variable called x. 
+
+# Print the value of the variable and its type.
+
